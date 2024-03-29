c6d19cfdd479465fa2d01c6543cb1e16fb7f1f5d
Salik-07 <salikhussain41@gmail.com>
2024-03-30 02:01:21 +0500

    Remove route that fetches user by its ID

--- src/routers/user.js
+++ src/routers/user.js
@@ -60,22 +60,6 @@ router.get("/api/v1/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get("/api/v1/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
 router.patch("/api/v1/users/:id", async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password", "age"];
