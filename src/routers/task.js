3c4a4268991fb7b76429d3cbf54da38bc3a888db
Salik-07 <salikhussain41@gmail.com>
2024-04-01 19:21:51 +0500

    Add auth middleware on all the remaining task's handlers

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,11 +19,21 @@ router.post("/api/v1/tasks", auth, async (req, res) => {
   }
 });
 
-router.get("/api/v1/tasks", async (req, res) => {
+router.get("/api/v1/tasks", auth, async (req, res) => {
   try {
-    const tasks = await Task.find();
+    // 1. Taken a user and find their tasks
+    await req.user.populate({
+      path: "tasks",
+    });
+    res.send(req.user.tasks);
 
-    res.send(tasks);
+    // 2. Taken a task and find the user who created task
+    // const tasks = await Task.find({ owner: req.user._id }).populate({
+    //   path: "owner",
+    //   select: "name",
+    // });
+
+    // res.send(tasks);
   } catch (e) {
     res.status(500).send();
   }
@@ -45,7 +55,7 @@ router.get("/api/v1/tasks/:id", auth, async (req, res) => {
   }
 });
 
-router.patch("/api/v1/tasks/:id", async (req, res) => {
+router.patch("/api/v1/tasks/:id", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["description", "completed"];
   const isValidOperation = updates.every((update) =>
@@ -57,7 +67,10 @@ router.patch("/api/v1/tasks/:id", async (req, res) => {
   }
 
   try {
-    const task = await Task.findById(req.params.id);
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
 
     if (!task) {
       return res.status(404).send();
@@ -72,9 +85,12 @@ router.patch("/api/v1/tasks/:id", async (req, res) => {
   }
 });
 
-router.delete("/api/v1/tasks/:id", async (req, res) => {
+router.delete("/api/v1/tasks/:id", auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id);
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
 
     if (!task) {
       return res.status(404).send();
